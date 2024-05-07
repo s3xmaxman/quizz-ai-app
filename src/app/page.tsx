944f4aae29a754ed7aba31da4602f0e9d84d2b29
s3xmaxman <badside3919@gmail.com>
2024-05-07 22:49:54 +0900

    Quiz UI Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Hello World👋</h1>
-    </main>
+    <div className="flex flex-col flex-1">
+      <main className="flex justify-center flex-1">
+        <h1 className="text-6xl font-bold">Hello World👋</h1>
+      </main>
+      <footer>
+        <Button>Start</Button>
+      </footer>
+    </div>
   )
 }
